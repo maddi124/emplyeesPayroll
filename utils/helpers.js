a4a2b8bb40a8a1fd3e64e03b8db3080cbc0d41f9
maddi124 <vmgarcia.cabrera21@gmail.com>
2022-10-01 23:00:20 -0400

    added connection and db

---
+++ utils/helpers.js
@@ -0,0 +1,11 @@
+module.exports = {
+    format_url: (url) => {
+      return url
+        .replace("http://", "")
+        .replace("https://", "")
+        .replace("www.", "")
+        .split("/")[0]
+        .split("?")[0];
+    },
+  };
+
