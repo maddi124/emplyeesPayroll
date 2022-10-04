662403c89ea8f0bbc320acf33601e3202f43cbde
maddi124 <vmgarcia.cabrera21@gmail.com>
2022-10-03 22:04:59 -0400

    added seeds

--- seeds/employee-seeds.js
+++ seeds/employee-seeds.js
@@ -1,5 +1,31 @@
 const { Employee } = require("../models");
 
+const employeeData =[
+    {
+        id:"1",
+        first_name:"Jose",
+        last_name:"Smith",
+        employee_id:"9383",
+        password:"password12345"
+    },
+    {
+        id:"2",
+        first_name:"Marie",
+        last_name:"Cross",
+        employee_id:"3456",
+        password:"password1234"
+    },
+    {
+        id:"3",
+        first_name:"Alex",
+        last_name:"Garcia",
+        employee_id:"7765",
+        password:"password12"
+    },
+    
+];
 
-
+const seedEmployees = () =>
+  Employee.bulkCreate(employeeData, { individualHooks: true });
+  
 module.exports = seedEmployees;
--- seeds/manager-seeds.js
+++ seeds/manager-seeds.js
@@ -1,5 +1,13 @@
 const { Manager } = require("../models");
 
+const managerData = [
+    {
+        email:"jp@gmail.com",
+        password:"password123",
 
+    },
+];
+const seedManagers = () =>
+    Manager.bulkCreate(managerData,{individualHooks: true});
 
 module.exports = seedManagers;
