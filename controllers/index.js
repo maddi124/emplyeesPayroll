b6e86f178906eee8ec41f26dbab3cfef33f9cd7f
maddi124 <vmgarcia.cabrera21@gmail.com>
2022-10-04 19:47:50 -0400

    connected the server

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 
-const apiRoutes = require('./api');
 const homeRoutes = require('./homepage-routes');
+
+router.use('/', homeRoutes);
 
 
 module.exports = router;
